ab8be21017b4eab88c13e1e2e099d51c65b92b9f
Olli Salonen <olli.salonen@iki.fi>
2024-11-18 21:01:36 +0200

    Update kostal_power_limit.py

--- kostal_power_limit.py
+++ kostal_power_limit.py
@@ -8,20 +8,15 @@ from pykoplenti import (ApiClient, AuthenticationException)
 from aiohttp import ClientSession
 urllib3.disable_warnings()
 
-# Change only the 3 lines below:
+# Just change the three lines below
 
-MARGINAL = 0.0031 # How much is the marginal that your electricity company takes
-PLENTICORE_IP = "192.168.1.3" # Inverter IP Address
-PLENTICORE_PASSWORD = "inverter_password"
+MARGINAL = 0.0031 # Marginal from your electricity company, note: in euros per kWh
+PLENTICORE_IP = "192.168.1.3"
+PLENTICORE_PASSWORD = "password"
 
-###################
+#####
 
 MAX = -1
-
-def check_http(resp):
-  if (resp.status_code > 299):
-    print(f'KSEM request failed. Response: {resp.status_code}')
-    exit(2)
 
 async def set_power_limit(limit):
   async with ClientSession() as session:
@@ -38,8 +33,6 @@ async def set_power_limit(limit):
     current_limit = device_local["Inverter:ActivePowerLimitation"]
     max_power = device_local["Inverter:MaxApparentPower"]
 
-    print(f"Active Power Limitation: {current_limit} / {max_power}\n")
-
     if (limit == MAX):
       wanted_limit = max_power
     else:
@@ -50,69 +43,20 @@ async def set_power_limit(limit):
       exit(0)
 
     await client.set_setting_values("devices:local", {"Inverter:ActivePowerLimitation":wanted_limit})
-
-    print(f'OK')
+    print(f'OK, limit set at {wanted_limit} W.')
     exit(0)
 
 
-  headers = {"Authorization": f'Bearer {KSEM_TOKEN}'}
-  response = requests.get(f'https://{KSEM_IP}/api/kostal-energyflow/configuration', headers = headers, verify = False)
-  check_http(response)
-
-  ksem_data = response.json()
-
-  if (limit == 0):
-    print("Disabling grid feed in.")
-    if (ksem_data["powerreduction"]["limit"] == 0):
-      print(f'Nothing to do, limit at 0 already.')
-    else:
-      print(f'Changing limit from {ksem_data["powerreduction"]["limit"]} to 0.')
-      ksem_data["powerreduction"]["limit"] = 0
-      ksem_data["powerreduction"]["enabled"] = True
-      ksem_data_str=json.dumps(ksem_data)
-      response = requests.put(f'https://{KSEM_IP}/api/kostal-energyflow/configuration', headers = headers, data = ksem_data_str.lower(), verify = False)
-      check_http(response)
-
-      response = requests.get(f'https://{KSEM_IP}/api/kostal-energyflow/configuration', headers = headers, verify = False)
-      check_http(response)
-      ksem_data = response.json()
-      if (ksem_data["powerreduction"]["limit"] == 0):
-        print("Operation verified.")
-        syslog.syslog("Grid feed in disabled due to low electricity price.")
-
-  elif (limit == MAX):
-    if (ksem_data["powerreduction"]["limit"] == ksem_data["powerreduction"]["maxpower"]):
-      print(f'Nothing to do, limit at {ksem_data["powerreduction"]["limit"]} (MAX) already.')
-    else:
-      print(f'Changing limit from {ksem_data["powerreduction"]["limit"]} to {ksem_data["powerreduction"]["maxpower"]} (MAX).')
-      ksem_data["powerreduction"]["limit"] = ksem_data["powerreduction"]["maxpower"]
-      ksem_data_str=json.dumps(ksem_data)
-      print(ksem_data_str.lower())
-      response = requests.put(f'https://{KSEM_IP}/api/kostal-energyflow/configuration', headers = headers, data = ksem_data_str.lower(), verify = False)
-      check_http(response)
-
-      response = requests.get(f'https://{KSEM_IP}/api/kostal-energyflow/configuration', headers = headers, verify = False)
-      check_http(response)
-      ksem_data = response.json()
-      if (ksem_data["powerreduction"]["limit"] == ksem_data["powerreduction"]["maxpower"]):
-        print("Operation verified.")
-        syslog.syslog("Grid feed in activated.")
-
-  else:
-    print("Something else.")
-
-time.sleep(3)
 response = requests.get('https://api.spot-hinta.fi/JustNow/')
 if (response.status_code != 200):
   print("Could not get spot price, disabling power limitation!")
-  set_power_limit(MAX)
+  asyncio.run(set_power_limit(MAX))
   exit(1)
 
 data = response.json()
 if data["PriceNoTax"] > MARGINAL:
-  print(f'Price is higher than marginal ({data["PriceNoTax"]} EUR/kWh), disabling feed in limit.')
+  print(f'Price is higher than marginal ({data["PriceNoTax"]} EUR/kWh), feed in limit disabled.')
   asyncio.run(set_power_limit(MAX))
 else:
   print(f'Price is too low ({data["PriceNoTax"]} EUR/kWh), limit feed in.')
-  print(data["PriceNoTax"])
-  asyncio.run(set_power_limit(0))
+  asyncio.run(set_power_limit("0"))
